e83bdd177a23cf8f7dcfd2c90371f0fa22b55fb6
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-15 13:33:40 -0600

    bob

---
+++ src/Bob.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello, My name is Bob. \nAsk me a question, nicely tho.");
+        String userInput = in.nextLine();
+
+        if(userInput.endsWith("!")) {
+            System.out.println("Woah, Chill out!");
+        } else if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine, Don't tell me");
+        } else {
+            System.out.println("That's cool");
+        }
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String sentence =
+                "We don't need no education " +
+                "We don't need no thought control";
+        System.out.println(sentence);
+
+        String sentence1 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(sentence1);
+
+        String sentence3 = "In windows, the main drive is usually C:\\ ";
+        System.out.println(sentence3);
+
+        String sentence4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(sentence4);
+
+    }
+}
