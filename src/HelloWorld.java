f5f8e45ab06ff279c8f9179dd1d94dfbdbcc2d5c
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-15 10:48:06 -0600

    ex 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "Hello";
+        System.out.println(myString);
+        myString = "Mario";
+        System.out.println(myString);
+        long myNumber = 123L;
+        float fNumber = 3.14f;
+        System.out.println(myNumber);
+        System.out.println(fNumber);
+
+        /*next ex*/
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+/*        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        int three = (int) "three";*/
+
+        x = 4;
+        x += 5;
+        System.out.println(x);
+
+        x = 3;
+        int y = 4;
+        y *= x;
+        System.out.println(y);
+
+        x = 10;
+        y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x + "" + y );
+
+        int maxValue = Integer.MAX_VALUE + 1;
+        System.out.println(maxValue);
     }
 }
