b582886838f6f84c7cdd86b0a0019f7dc2fde752
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-10 11:03:37 -0600

    IO append

--- src/notes/JavaIONotes.java
+++ src/notes/JavaIONotes.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JavaIONotes {
@@ -23,5 +26,31 @@ public class JavaIONotes {
         System.out.println(Files.exists(myPath));
 
         printPoem(myPath);
+
+        String directory = "./src/notes/data";
+
+        String fileName = "groceryList.txt";
+
+        Path dataDir = Paths.get(directory);
+
+        Path dataDirAndFile = Paths.get(directory, fileName);
+
+        if(Files.notExists(dataDir)) {
+            Files.createDirectories(dataDir);
+        }
+        if(Files.notExists(dataDirAndFile)) {
+            Files.createFile(dataDirAndFile);
+        }
+
+        List<String> groceries = Arrays.asList("coffee", "milk", "dogFood", "bread");
+
+        System.out.println(groceries);
+
+        Files.write(dataDirAndFile, groceries);
+
+        List<String> cheese = Arrays.asList("cheedar cheese", "Colby Jack");
+
+        Files.write(dataDirAndFile, cheese, StandardOpenOption.APPEND);
+
     }
 }
